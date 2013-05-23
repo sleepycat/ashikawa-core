e39aae898e3bba1125653cb88e4df47fc1557ccb
moonglum <moonglum@moonbeamlabs.com>
2013-05-23 17:46:57 +0200

    Added new figure for 1.3

--- spec/unit/figure_spec.rb
+++ spec/unit/figure_spec.rb
@@ -22,33 +22,40 @@ describe Ashikawa::Core::Figure do
       },
       "shapes" => {
         "count" => 2
+      },
+      "attributes" => {
+        "count" => 12
       }
     }
   }
   subject { Ashikawa::Core::Figure.new(raw_figures) }
 
   it "should check for the alive figures" do
-      subject.alive_size.should == 0
-      subject.alive_count.should == 0
+    subject.alive_size.should == 0
+    subject.alive_count.should == 0
   end
 
   it "should check for the dead figures" do
-      subject.dead_size.should == 2384
-      subject.dead_count.should == 149
-      subject.dead_deletion.should == 0
+    subject.dead_size.should == 2384
+    subject.dead_count.should == 149
+    subject.dead_deletion.should == 0
   end
 
   it "should check for the datafiles figures" do
-      subject.datafiles_count.should == 1
-      subject.datafiles_file_size.should == 124
+    subject.datafiles_count.should == 1
+    subject.datafiles_file_size.should == 124
   end
 
   it "should check for the journal figures" do
-      subject.journals_count.should == 1
-      subject.journals_file_size.should == 124
+    subject.journals_count.should == 1
+    subject.journals_file_size.should == 124
   end
 
   it "should check for the shapes figure" do
-      subject.shapes_count.should == 2
+    subject.shapes_count.should == 2
+  end
+
+  it "should check for the attributes_count figure" do
+    subject.attributes_count.should == 12
   end
 end
