2690db03719b48e0968f9ffc08f156fa39f3b5f1
moonglum <moonglum@moonbeamlabs.com>
2013-09-15 22:19:31 +0200

    Cursor Spec: First Step

--- spec/unit/cursor_spec.rb
+++ spec/unit/cursor_spec.rb
@@ -4,33 +4,24 @@ require 'ashikawa-core/cursor'
 
 describe Ashikawa::Core::Cursor do
   subject { Ashikawa::Core::Cursor }
+  let(:database) { double }
 
-  before :each do
-    @database = double
-    double Ashikawa::Core::Document
-  end
+  describe "cursor for a non-complete batch" do
+    let(:response) { server_response("cursor/26011191") }
+    subject { Ashikawa::Core::Cursor.new(database, response) }
 
-  it "should create a cursor for a non-complete batch" do
-    my_cursor = subject.new @database, server_response("cursor/26011191")
-    expect(my_cursor.id).to        eq("26011191")
-    expect(my_cursor.length).to    eq(5)
-  end
+    its(:id) { should eq("26011191") }
+    its(:length) { should eq(5) }
 
-  it "should create a cursor for the last batch" do
-    my_cursor = subject.new @database, server_response("cursor/26011191-3")
-    expect(my_cursor.id).to be_nil
-    expect(my_cursor.length).to eq(5)
-  end
-
-  describe "existing cursor" do
-    subject { Ashikawa::Core::Cursor.new @database,
-      server_response("cursor/26011191")
-    }
+    it "should be deletable" do
+      expect(database).to receive(:send_request).with("cursor/26011191", delete: {})
+      subject.delete
+    end
 
     it "should iterate over all documents of a cursor when given a block" do
       first = true
 
-      allow(@database).to receive(:send_request).with("cursor/26011191", put: {}) do
+      allow(database).to receive(:send_request).with("cursor/26011191", put: {}) do
         if first
           first = false
           server_response("cursor/26011191-2")
@@ -38,18 +29,18 @@ describe Ashikawa::Core::Cursor do
           server_response("cursor/26011191-3")
         end
       end
-      expect(@database).to receive(:send_request).twice
+      expect(database).to receive(:send_request)
+        .twice
+      expect(Ashikawa::Core::Document).to receive(:new)
+        .exactly(5).times
 
-      allow(Ashikawa::Core::Document).to receive(:new)
-      expect(Ashikawa::Core::Document).to receive(:new).exactly(5).times
-
-      subject.each { |document| }
+      subject.each { }
     end
 
     it "should return an enumerator to go over all documents of a cursor when given no block" do
       first = true
 
-      allow(@database).to receive(:send_request).with("cursor/26011191", put: {}) do
+      allow(database).to receive(:send_request).with("cursor/26011191", put: {}) do
         if first
           first = false
           server_response("cursor/26011191-2")
@@ -57,10 +48,10 @@ describe Ashikawa::Core::Cursor do
           server_response("cursor/26011191-3")
         end
       end
-      expect(@database).to receive(:send_request).twice
-
-      allow(Ashikawa::Core::Document).to receive(:new)
-      expect(Ashikawa::Core::Document).to receive(:new).exactly(5).times
+      expect(database).to receive(:send_request)
+        .twice
+      expect(Ashikawa::Core::Document).to receive(:new)
+        .exactly(5).times
 
       enumerator = subject.each
       enumerator.next
@@ -71,44 +62,46 @@ describe Ashikawa::Core::Cursor do
       expect { enumerator.next }.to raise_exception(StopIteration)
     end
 
-    it "should be deletable" do
-      allow(@database).to receive(:send_request)
-      expect(@database).to receive(:send_request).with("cursor/26011191",
-        delete: {})
-
-      subject.delete
-    end
-
     it "should be enumerable" do
       first = true
+      result = double
 
-      allow(@database).to receive(:send_request).with("cursor/26011191", put: {}) do
-        if first
-          first = false
-          server_response("cursor/26011191-2")
-        else
-          server_response("cursor/26011191-3")
+      expect(database).to receive(:send_request)
+        .twice
+        .with("cursor/26011191", put: {}) do
+          if first
+            first = false
+            server_response("cursor/26011191-2")
+          else
+            server_response("cursor/26011191-3")
+          end
         end
-      end
-      expect(@database).to receive(:send_request).twice
 
-      allow(Ashikawa::Core::Document).to receive(:new).and_return { 1 }
-      expect(Ashikawa::Core::Document).to receive(:new).exactly(5).times
+      expect(Ashikawa::Core::Document).to receive(:new)
+        .exactly(5).times
+        .and_return(result)
 
-      expect(subject.map{|i| i}[0]).to eq(1)
+      expect(subject.map{|i| i}[0]).to eq(result)
     end
 
     it "should return edge objects when recieving data from an edge collection" do
-      allow(@database).to receive(:send_request).with("cursor/26011191", put: {}) do
-        server_response("cursor/edges")
-      end
-      expect(@database).to receive(:send_request).once
+      expect(database).to receive(:send_request)
+        .once
+        .with("cursor/26011191", put: {})
+        .and_return(server_response("cursor/edges"))
 
-      allow(Ashikawa::Core::Edge).to receive(:new).and_return { 1 }
-      expect(Ashikawa::Core::Edge).to receive(:new).exactly(2).times
+      expect(Ashikawa::Core::Edge).to receive(:new)
+        .exactly(2).times
 
-      subject.each { |document| }
+      subject.each { }
     end
+  end
 
+  describe "cursor for the last batch" do
+    let(:response) { server_response("cursor/26011191-3") }
+    subject { Ashikawa::Core::Cursor.new(database, response) }
+
+    its(:id) { should be_nil }
+    its(:length) { should eq(5) }
   end
 end
