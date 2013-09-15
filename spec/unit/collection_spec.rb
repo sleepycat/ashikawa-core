84bb433a529874dd3147b029fd8ae63d5d5b23f1
moonglum <moonglum@moonbeamlabs.com>
2013-09-15 20:02:51 +0200

    Collection Spec: More cleanup

--- spec/unit/collection_spec.rb
+++ spec/unit/collection_spec.rb
@@ -16,48 +16,68 @@ describe Ashikawa::Core::Collection do
 
     let(:raw_key_options) { double }
     let(:key_options) { double }
+    let(:response) { double }
 
     its(:name) { should eq("example_1") }
     its(:id) { should eq("60768679") }
 
     it "should check if the collection waits for sync" do
+      allow(response).to receive(:[])
+        .with("waitForSync")
+        .and_return(true)
       expect(database).to receive(:send_request)
         .with("collection/60768679/properties", {})
-        .and_return { server_response("collections/60768679-properties") }
+        .and_return(response)
 
       expect(subject.wait_for_sync?).to be_true
     end
 
     it "should know how many documents the collection has" do
+      allow(response).to receive(:[])
+        .with("count")
+        .and_return(54)
       expect(database).to receive(:send_request)
         .with("collection/60768679/count", {})
-        .and_return { server_response("collections/60768679-count") }
+        .and_return(response)
 
       expect(subject.length).to eq(54)
     end
 
     it "should know if the collection is volatile" do
+      allow(response).to receive(:[])
+        .with("isVolatile")
+        .and_return(true)
       expect(database).to receive(:send_request)
         .with("collection/60768679/properties", {})
-        .and_return { server_response("collections/60768679-properties-volatile") }
+        .and_return(response)
 
       expect(subject.volatile?).to be_true
     end
 
     it "should know if the collection is not volatile" do
+      allow(response).to receive(:[])
+        .with("isVolatile")
+        .and_return(false)
       expect(database).to receive(:send_request)
         .with("collection/60768679/properties", {})
-        .and_return { server_response("collections/60768679-properties") }
+        .and_return(response)
 
       expect(subject.volatile?).to be_false
     end
 
     it "should check for the figures" do
+      figures = double
+
+      allow(response).to receive(:[])
+        .with("figures")
+        .and_return(figures)
       expect(database).to receive(:send_request)
         .with("collection/60768679/figures", {})
-        .and_return { server_response("collections/60768679-figures") }
+        .and_return(response)
 
-      expect(Ashikawa::Core::Figure).to receive(:new).exactly(1).times.with(server_response("collections/60768679-figures")["figures"])
+      expect(Ashikawa::Core::Figure).to receive(:new)
+        .exactly(1).times
+        .with(figures)
 
       subject.figure
     end
@@ -109,7 +129,6 @@ describe Ashikawa::Core::Collection do
       allow(Ashikawa::Core::KeyOptions).to receive(:new)
         .with(raw_key_options)
         .and_return { key_options }
-
       expect(database).to receive(:send_request)
         .with("collection/60768679/properties", {})
         .and_return { { "keyOptions" => raw_key_options } }
@@ -128,7 +147,7 @@ describe Ashikawa::Core::Collection do
       it "should receive a document by ID via fetch" do
         expect(database).to receive(:send_request)
           .with("document/60768679/333", {})
-          .and_return { server_response('documents/example_1-137249191') }
+          .and_return(double)
         expect(Ashikawa::Core::Document).to receive(:new)
 
         subject.fetch(333)
@@ -137,7 +156,7 @@ describe Ashikawa::Core::Collection do
       it "should receive a document by ID via []" do
         expect(database).to receive(:send_request)
           .with("document/60768679/333", {})
-          .and_return { server_response('documents/example_1-137249191') }
+          .and_return(double)
         expect(Ashikawa::Core::Document).to receive(:new)
 
         subject[333]
@@ -168,33 +187,34 @@ describe Ashikawa::Core::Collection do
     end
 
     describe "indexes" do
+      let(:index_response) { double }
+
       it "should add a new index" do
         expect(database).to receive(:send_request)
           .with("index?collection=60768679", post: {
             "type" => "hash", "fields" => [ "a", "b" ]
           })
-          .and_return { server_response('indices/new-hash-index') }
+          .and_return(index_response)
+        expect(Ashikawa::Core::Index).to receive(:new)
+          .with(subject, index_response)
 
-        expect(Ashikawa::Core::Index).to receive(:new).with(subject,
-          server_response('indices/new-hash-index'))
-
-        subject.add_index :hash, on: [ :a, :b ]
+        subject.add_index(:hash, on: [ :a, :b ])
       end
 
       it "should get an index by ID" do
         allow(database).to receive(:send_request).with("index/example_1/168054969")
-          .and_return { server_response('indices/hash-index') }
+          .and_return(index_response)
+        expect(Ashikawa::Core::Index).to receive(:new)
+          .with(subject, index_response)
 
-        expect(Ashikawa::Core::Index).to receive(:new).with(subject,
-          server_response('indices/hash-index'))
-
-        subject.index 168054969
+        subject.index(168054969)
       end
 
+      # TODO: Redo this
       it "should get all indexes" do
         allow(database).to receive(:send_request)
           .with("index?collection=60768679")
-          .and_return { server_response('indices/all') }
+          .and_return(double) { server_response('indices/all') }
 
         expect(Ashikawa::Core::Index).to receive(:new)
           .exactly(1).times
@@ -211,15 +231,15 @@ describe Ashikawa::Core::Collection do
 
     it "should create a new document" do
       document = double
-      server_response = double
+      response = double
       raw_document = double
 
       allow(database).to receive(:send_request)
         .with("document?collection=60768679", post: raw_document)
-        .and_return(server_response)
+        .and_return(response)
 
       expect(Ashikawa::Core::Document).to receive(:new)
-        .with(database, server_response, raw_document)
+        .with(database, response, raw_document)
         .and_return(document)
 
       subject.create_document(raw_document)
@@ -240,7 +260,7 @@ describe Ashikawa::Core::Collection do
     it "should receive an edge by ID" do
       expect(database).to receive(:send_request)
         .with("edge/60768679/333", {})
-        .and_return { server_response('documents/example_1-137249191') }
+        .and_return(double)
       expect(Ashikawa::Core::Edge).to receive(:new)
 
       subject.fetch(333)
@@ -254,16 +274,16 @@ describe Ashikawa::Core::Collection do
     end
 
     it "should create a new edge" do
-      server_response = double
+      response = double
       raw_document = double
       document = double
 
       allow(database).to receive(:send_request)
         .with("edge?collection=60768679&from=1&to=2", post: raw_document)
-        .and_return(server_response)
+        .and_return(response)
 
       expect(Ashikawa::Core::Edge).to receive(:new)
-        .with(database, server_response, raw_document)
+        .with(database, response, raw_document)
         .and_return(document)
 
       subject.create_edge(double(id: "1"), double(id: "2"), raw_document)
