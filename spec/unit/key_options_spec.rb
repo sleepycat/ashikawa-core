cbd87971e2c9954469992051249ef69a1087e78b
moonglum <moonglum@moonbeamlabs.com>
2014-05-23 11:18:00 -0700

    Clean up key_options_spec

--- spec/unit/key_options_spec.rb
+++ spec/unit/key_options_spec.rb
@@ -3,25 +3,22 @@ require 'unit/spec_helper'
 require 'ashikawa-core/key_options'
 
 describe Ashikawa::Core::KeyOptions do
-  let(:type) { double }
-  let(:offset) { double }
-  let(:increment) { double }
-  let(:allow_user_keys) { double }
-  let(:raw_key_options) do
-    {
+  let(:type) { :autoincrement }
+  let(:offset) { 12 }
+  let(:increment) { 2 }
+  let(:allow_user_keys) { double('Boolean') }
+
+  subject do
+    Ashikawa::Core::KeyOptions.new(
       'type' => type,
       'offset' => offset,
       'increment' => increment,
       'allowUserKeys' => allow_user_keys
-    }
+    )
   end
 
-  describe 'initialized key options' do
-    subject { Ashikawa::Core::KeyOptions.new(raw_key_options) }
-
-    its(:type) { should eq(type) }
-    its(:offset) { should eq(offset) }
-    its(:increment) { should eq(increment) }
-    its(:allow_user_keys) { should eq(allow_user_keys) }
-  end
+  its(:type) { should eq(type) }
+  its(:offset) { should eq(offset) }
+  its(:increment) { should eq(increment) }
+  its(:allow_user_keys) { should eq(allow_user_keys) }
 end
