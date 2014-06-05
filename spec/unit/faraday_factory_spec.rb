d4735972712a3d7d125191240ecf4550755d52e3
moonglum <moonglum@moonbeamlabs.com>
2014-06-05 15:57:52 +0200

    Extracted a spec for the FaradayFactory

---
+++ spec/unit/faraday_factory_spec.rb
@@ -0,0 +1,35 @@
+# -*- encoding : utf-8 -*-
+require 'unit/spec_helper'
+require 'ashikawa-core/faraday_factory'
+
+describe Ashikawa::Core::FaradayFactory do
+  describe 'initializing Faraday' do
+    subject { Ashikawa::Core::FaradayFactory }
+    let(:adapter) { double('Adapter') }
+    let(:logger) { double('Logger') }
+    let(:blocky) { double('Block') }
+    let(:api_string) { double('ApiString') }
+
+    it 'should initalize with specific logger and adapter' do
+      expect(Faraday).to receive(:new).with(api_string).and_yield(blocky)
+      expect(blocky).to receive(:request).with(:json)
+      expect(blocky).to receive(:response).with(:logger, logger)
+      expect(blocky).to receive(:response).with(:error_response)
+      expect(blocky).to receive(:response).with(:json)
+      expect(blocky).to receive(:adapter).with(adapter)
+
+      subject.create_connection(api_string, adapter: adapter, logger: logger)
+    end
+
+    it 'should initialize with defaults when no specific logger and adapter was given' do
+      expect(Faraday).to receive(:new).with(api_string).and_yield(blocky)
+      expect(blocky).to receive(:request).with(:json)
+      expect(blocky).to receive(:response).with(:logger, NullLogger.instance)
+      expect(blocky).to receive(:response).with(:error_response)
+      expect(blocky).to receive(:response).with(:json)
+      expect(blocky).to receive(:adapter).with(Faraday.default_adapter)
+
+      subject.create_connection(api_string, {})
+    end
+  end
+end
