cf5e53b296706845bdca740dcc2d5b35fadfa393
moonglum <moonglum@moonbeamlabs.com>
2014-06-06 14:03:50 +0200

    FaradayFactory refactored

--- lib/ashikawa-core/faraday_factory.rb
+++ lib/ashikawa-core/faraday_factory.rb
@@ -6,6 +6,8 @@ module Ashikawa
   module Core
     # Create Faraday objects
     class FaradayFactory
+      DEFAULTS = { additional_request_middlewares: [], additional_response_middlewares: [], adapter: Faraday.default_adapter }
+
       # Create a Faraday object
       #
       # @param [String] url The complete URL of the ArangoDB instance
@@ -15,33 +17,30 @@ module Ashikawa
       # @example Create a FaradayObject with the given configuration
       #  faraday = FaradayFactory.new('http://localhost:8529/_db/mydb/_api', logger: my_logger)
       def self.create_connection(url, options)
-        faraday = new
+        options = DEFAULTS.merge(options)
+        faraday = new(options.fetch(:additional_request_middlewares), options.fetch(:additional_response_middlewares))
         faraday.debug_headers = options.fetch(:debug_headers) { false }
-        faraday.request_middlewares += options.fetch(:additional_request_middlewares, [])
-        faraday.response_middlewares += options.fetch(:additional_response_middlewares, [])
         faraday.logger = options.fetch(:logger) if options.has_key?(:logger)
-        faraday.adapter = options.fetch(:adapter) { Faraday.default_adapter }
+        faraday.adapter = options.fetch(:adapter)
         faraday.faraday_for(url)
       end
 
       attr_accessor :debug_headers
-      attr_accessor :request_middlewares
-      attr_accessor :response_middlewares
       attr_accessor :adapter
 
-      def initialize
-        @request_middlewares = [:json]
-        @response_middlewares = [:error_response, :json]
+      def initialize(additional_request_middlewares, additional_response_middlewares)
+        @request_middlewares = [:json] + additional_request_middlewares
+        @response_middlewares = [:error_response, :json] + additional_response_middlewares
       end
 
       def logger=(logger)
-        response_middlewares << [:minimal_logger, logger, debug_headers: debug_headers]
+        @response_middlewares << [:minimal_logger, logger, debug_headers: debug_headers]
       end
 
       def faraday_for(url)
         Faraday.new(url) do |connection|
-          request_middlewares.each { |middleware| connection.request(*middleware) }
-          response_middlewares.each { |middleware| connection.response(*middleware) }
+          @request_middlewares.each { |middleware| connection.request(*middleware) }
+          @response_middlewares.each { |middleware| connection.response(*middleware) }
           connection.adapter(*adapter)
         end
       end
