a7582bb2eaef376bc45a06be204eccaf8b421345
moonglum <moonglum@moonbeamlabs.com>
2014-06-10 17:29:43 +0200

    Rename blocky to a saner variable name in FaradayFactory spec

--- spec/unit/faraday_factory_spec.rb
+++ spec/unit/faraday_factory_spec.rb
@@ -5,46 +5,46 @@ require 'ashikawa-core/faraday_factory'
 describe Ashikawa::Core::FaradayFactory do
   describe 'FaradayFactory.create_connection' do
     subject { Ashikawa::Core::FaradayFactory }
-    let(:blocky) { double('Block') }
+    let(:config_block) { double('Block') }
     let(:api_string) { double('ApiString') }
 
     before do
-      allow(Faraday).to receive(:new).with(api_string).and_yield(blocky)
-      allow(blocky).to receive(:request).with(:json)
-      allow(blocky).to receive(:response).with(:error_response)
-      allow(blocky).to receive(:response).with(:json)
-      allow(blocky).to receive(:adapter).with(Faraday.default_adapter)
+      allow(Faraday).to receive(:new).with(api_string).and_yield(config_block)
+      allow(config_block).to receive(:request).with(:json)
+      allow(config_block).to receive(:response).with(:error_response)
+      allow(config_block).to receive(:response).with(:json)
+      allow(config_block).to receive(:adapter).with(Faraday.default_adapter)
     end
 
     it 'should initialize with a specific logger' do
       logger = double('Logger')
-      expect(blocky).to receive(:response).with(:minimal_logger, logger, debug_headers: false)
+      expect(config_block).to receive(:response).with(:minimal_logger, logger, debug_headers: false)
       subject.create_connection(api_string, logger: logger)
     end
 
     it 'should initialize with a specific adapter' do
       adapter = double('Adapter')
-      expect(blocky).to receive(:adapter).with(adapter)
+      expect(config_block).to receive(:adapter).with(adapter)
       subject.create_connection(api_string, adapter: adapter)
     end
 
     it 'should initialize with the default adapter when no specific adapter was given' do
-      expect(blocky).to receive(:adapter).with(Faraday.default_adapter)
+      expect(config_block).to receive(:adapter).with(Faraday.default_adapter)
       subject.create_connection(api_string, {})
     end
 
     it 'should allow to add additional request middlewares with options' do
-      expect(blocky).to receive(:request).with(:my_middleware, :options)
+      expect(config_block).to receive(:request).with(:my_middleware, :options)
       subject.create_connection(api_string, additional_request_middlewares: [[:my_middleware, :options]])
     end
 
     it 'should allow to add additional response middlewares with options' do
-      expect(blocky).to receive(:response).with(:my_middleware, :options)
+      expect(config_block).to receive(:response).with(:my_middleware, :options)
       subject.create_connection(api_string, additional_response_middlewares: [[:my_middleware, :options]])
     end
 
     it 'should allow to add additional middlewares with options' do
-      expect(blocky).to receive(:use).with(:my_middleware, :options)
+      expect(config_block).to receive(:use).with(:my_middleware, :options)
       subject.create_connection(api_string, additional_middlewares: [[:my_middleware, :options]])
     end
   end
