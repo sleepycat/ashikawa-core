b66e10e3b15fa9594c1ff0bea249af63022e7326
Dirk Breuer <dirk.breuer@gmail.com>
2014-09-03 11:31:32 +0200

    Graph: Add basic with database reference and name

--- lib/ashikawa-core/graph.rb
+++ lib/ashikawa-core/graph.rb
@@ -8,6 +8,44 @@ module Ashikawa
     #       through their corresponding graph class. Not doing so will eventually lead to inconsistency
     #       and data corruption.
     class Graph
+      # The database the Graph belongs to
+      #
+      # @return [Database] The associated database
+      # @api public
+      # @example
+      #   database = Ashikawa::Core::Database.new('http://localhost:8529')
+      #   raw_graph = {
+      #     'name' => 'example_1',
+      #     'edgeDefinitions' => [],
+      #     'orphanCollections' => []
+      #   }
+      #   graph = Ashikawa::Core::Graph.new(database, raw_collection)
+      #   graph.database #=> #<Database: ...>
+      attr_reader :database
+
+      # The name of the database
+      #
+      # @return [String] The name of the graph
+      # @api public
+      # @example
+      #   database = Ashikawa::Core::Database.new('http://localhost:8529')
+      #   raw_graph = {
+      #     'name' => 'example_1',
+      #     'edgeDefinitions' => [],
+      #     'orphanCollections' => []
+      #   }
+      #   graph = Ashikawa::Core::Graph.new(database, raw_collection)
+      #   graph.name #=> 'example_1
+      attr_reader :name
+
+      # Initialize a new graph instance
+      #
+      # @param [Database] database A reference to the database this graph belongs to
+      # @param [Hash] raw_graph The parsed JSON response from the database representing the graph
+      def initialize(database, raw_graph)
+        @database = database
+        @name     = raw_graph['name'] || raw_graph['_key']
+      end
     end
   end
 end
--- spec/unit/graph_spec.rb
+++ spec/unit/graph_spec.rb
@@ -1,7 +1,30 @@
 # -*- encoding : utf-8 -*-
 require 'unit/spec_helper'
 require 'ashikawa-core/graph'
+require 'ashikawa-core/database'
 
 describe Ashikawa::Core::Graph do
+  let(:database) { instance_double('Ashikawa::Core::Database') }
+  let(:raw_graph) { double('RawGraph').as_null_object }
 
+  context 'an initialized graph' do
+    subject { Ashikawa::Core::Graph.new(database, raw_graph) }
+
+    it 'should know its database' do
+      expect(subject.database).to eq database
+    end
+
+    it 'should know the name of the graph' do
+      allow(raw_graph).to receive(:[]).with('name').and_return('my_graph')
+      expect(subject.name).to eq 'my_graph'
+    end
+
+    it 'should extract the name from the _key if no name was provided' do
+      allow(raw_graph).to receive(:[]).with('name').and_return(nil)
+      allow(raw_graph).to receive(:[]).with('_key').and_return('my_graph')
+      expect(subject.name).to eq 'my_graph'
+    end
+
+    it 'should'
+  end
 end
