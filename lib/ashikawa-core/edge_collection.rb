114f171f09d1c7e06a4ae32d84380e45b11b3320
Dirk Breuer <dirk.breuer@gmail.com>
2014-09-04 15:19:02 +0200

    EdgeCollection: Added basic implementation

--- lib/ashikawa-core/edge_collection.rb
+++ lib/ashikawa-core/edge_collection.rb
@@ -3,7 +3,28 @@ require 'ashikawa-core/collection'
 
 module Ashikawa
   module Core
+    # An edge collection as it is returned from a graph
+    #
+    # @note This is basically just a regular collection with some additional attributes and methods to ease
+    #       working with collections in the graph module.
     class EdgeCollection < Collection
+      # The Graph instance this EdgeCollection was originally fetched from
+      #
+      # @return [Graph] The Graph instance the collection was fetched from
+      # @api public
+      attr_reader :graph
+
+      # Create a new EdgeCollection object
+      #
+      # @param [Database] database The database the connection belongs to
+      # @param [Hash] raw_collection The raw collection returned from the server
+      # @param [Graph] graph The graph from which this collection was fetched
+      # @note You should not create instance manually but rather use Graph#add_edge_definition
+      # @api public
+      def initialize(database, raw_collection, graph)
+        super(database, raw_collection)
+        @graph = graph
+      end
     end
   end
 end
--- spec/unit/edge_collection_spec.rb
+++ spec/unit/edge_collection_spec.rb
@@ -5,6 +5,29 @@ require 'ashikawa-core/graph'
 require 'ashikawa-core/database'
 
 describe Ashikawa::Core::EdgeCollection do
-  pending
+  let(:database) { instance_double('Ashikawa::Core::Database') }
+  let(:raw_collection) do
+    {
+      'id' => '60768679',
+      'name' => 'example_1',
+      'status' => 3,
+      'type' => 3,
+      'error' => false,
+      'code' => 200
+    }
+  end
+  let(:graph) { instance_double('Ashikawa::Core::Graph') }
+
+  context 'an initialized edge collection' do
+    subject { Ashikawa::Core::EdgeCollection.new(database, raw_collection, graph) }
+
+    it 'should be a subclass of Collection' do
+      expect(subject).to be_kind_of Ashikawa::Core::Collection
+    end
+
+    it 'should have a reference to its graph' do
+      expect(subject.graph).to eq graph
+    end
+  end
 end
 
