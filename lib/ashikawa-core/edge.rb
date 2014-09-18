b52d56e3187e2d0efc25f04a214121f198e348bf
Dirk Breuer <dirk.breuer@gmail.com>
2014-09-18 14:55:44 +0200

    Edge: If a graph is present pass it through the graph module

--- lib/ashikawa-core/edge.rb
+++ lib/ashikawa-core/edge.rb
@@ -56,7 +56,11 @@ module Ashikawa
       # @return [Hash] The parsed response from the server
       # @api private
       def send_request_for_document(opts)
-        @database.send_request("edge/#{@id}", opts)
+        if graph
+          @database.send_request("gharial/#{graph.name}/edge/#{@id}", opts)
+        else
+          @database.send_request("edge/#{@id}", opts)
+        end
       end
     end
   end
